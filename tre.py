931bc6bb336e76d2d20a0d2e9bd4d672c499cf6c
BernasconiLorenzo06 <bernasconi.lorenzo@iisgalvanimi.edu.it>
2023-03-15 12:58:00 +0000

    terzo commit

---
+++ tre.py
@@ -0,0 +1,27 @@
+from flask import Flask,render_template
+app = Flask(__name__)
+
+import datetime
+
+@app.route('/', methods=['GET'])
+def home():
+    return render_template('tre.html')
+
+@app.route('/pizza', methods=['GET'])
+def pizza():
+    return render_template('pizza.html')
+    
+@app.route('/sushi', methods=['GET'])
+def sushi():
+    return render_template('sushi.html')
+
+@app.route('/pasta', methods=['GET'])
+def pasta():
+    return render_template('pasta.html')
+
+@app.route('/hamburger', methods=['GET'])
+def hamburger():
+    return render_template('hamburger.html')
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=3245, debug=True)
